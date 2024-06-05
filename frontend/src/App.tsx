8abde432d4fa12ee68c64785e7c15f7227b73b09
JaehongDev <println@kakao.com>
2024-06-05 15:59:03 +0900

    feat: queryClient 적용

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
-import {Button, Card} from "@nextui-org/react";
+import {Card} from "@nextui-org/react";
 
 function App() {
   return (
-    <div className="container mx-auto">
-      <Card>
-        <Button color="primary">1234</Button>
+    <div className='container mx-auto h-screen'>
+      <Card className='w-[50vh] h-[50vh]'>
+        <div>면접 질문 시작</div>
       </Card>
     </div>
   )
--- frontend/src/api/AxiosInstance.ts
+++ frontend/src/api/AxiosInstance.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import {NETWORK} from "../constants/api.ts";
+import {NETWORK} from "@/constants/api";
 
 export const axiosInstance = axios.create({
   baseURL: 'http://localhost:8080/api/v1',
