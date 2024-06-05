bfaa1b2daf85948f9e7a2774c60509e06126426d
JaehongDev <println@kakao.com>
2024-06-05 15:04:36 +0900

    feat: axios 기본 설정

---
+++ frontend/src/api/AxiosInstance.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+import {NETWORK} from "../constants/api.ts";
+
+export const axiosInstance = axios.create({
+  baseURL: 'http://localhost:8080/api/v1',
+  timeout: NETWORK.TIMEOUT,
+  withCredentials: true
+})
---
+++ frontend/src/constants/api.ts
@@ -0,0 +1,5 @@
+export const NETWORK = {
+  RETRY_COUNT: 2,
+  TIMEOUT: 10000
+} as const;
+
