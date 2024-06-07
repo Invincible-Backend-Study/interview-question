a97699e9db779dc28bb2ea4f47ae2b00ec10b84f
JaehongDev <println@kakao.com>
2024-06-08 02:30:28 +0900

    feat: Exception code 추가

---
+++ backend/src/main/java/in/backend/domain/exception/DomainExceptionCode.java
@@ -0,0 +1,31 @@
+package in.backend.domain.exception;
+
+
+import java.util.function.Supplier;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum DomainExceptionCode {
+    QUESTION(100, "질문"),
+
+    QUESTION_SET(200, "질문 집합"),
+    QUESTION_SET_RULES_GENERATION_FAIL(QUESTION_SET.code + 1, "질문 집합 생성을 실패했습니다.");
+
+    private final int code;
+    private final String message;
+
+
+    public void invoke() {
+        throw new DomainException(this.code, this.message);
+    }
+
+    public void invokeByCondition(Supplier<Boolean> supplier) {
+        this.invokeByCondition(supplier.get());
+    }
+
+    public void invokeByCondition(boolean condition) {
+        if (condition) {
+            invoke();
+        }
+    }
+}
