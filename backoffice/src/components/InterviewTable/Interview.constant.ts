765111685ea00302739e10e6a8954c2b4842e494
JaehongDev <println@kakao.com>
2024-06-07 21:15:41 +0900

    feat: 인터뷰 테이블 UI 구성

---
+++ backoffice/src/components/InterviewTable/Interview.constant.ts
@@ -0,0 +1,12 @@
+import {Header} from "@/types/Table";
+
+const HEADERS: Header[] = [
+  {name: "질문 아이디", uid: "id", sortable: true},
+  {name: "질문 내용", uid: "content", sortable: true},
+  {name: "질문 작성자", uid: "author", sortable: true},
+]
+
+
+export const INTERVIEW = {
+  HEADERS
+}
