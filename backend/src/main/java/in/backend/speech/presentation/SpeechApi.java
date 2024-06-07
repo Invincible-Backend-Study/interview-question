26002fffc97c4a8420dd8b1e3255fa9ad0f5a12a
JaehongDev <println@kakao.com>
2024-06-07 19:10:36 +0900

    feat: 음성인식 api 추가

--- backend/src/main/java/in/backend/speech/presentation/SpeechApi.java
+++ backend/src/main/java/in/backend/speech/presentation/SpeechApi.java
@@ -4,6 +4,9 @@ package in.backend.speech.presentation;
 import static java.util.stream.Collectors.collectingAndThen;
 import static java.util.stream.Collectors.joining;
 
+import com.google.api.gax.core.CredentialsProvider;
+import com.google.api.gax.core.FixedCredentialsProvider;
+import com.google.auth.oauth2.ServiceAccountCredentials;
 import com.google.cloud.speech.v1.RecognitionAudio;
 import com.google.cloud.speech.v1.RecognitionConfig;
 import com.google.cloud.speech.v1.RecognitionConfig.AudioEncoding;
@@ -11,9 +14,11 @@ import com.google.cloud.speech.v1.RecognizeResponse;
 import com.google.cloud.speech.v1.SpeechClient;
 import com.google.cloud.speech.v1.SpeechRecognitionAlternative;
 import com.google.cloud.speech.v1.SpeechRecognitionResult;
+import com.google.cloud.speech.v1.SpeechSettings;
 import com.google.protobuf.ByteString;
 import in.backend.speech.payload.request.SpeechRecognizeRequest;
 import in.backend.speech.payload.response.SpeechRecognizeResponse;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -32,25 +37,36 @@ public class SpeechApi {
     @PostMapping("/recognize")
     public SpeechRecognizeResponse recognize(
             @RequestBody SpeechRecognizeRequest request
-    ) {
+    ) throws IOException {
 
-        try (SpeechClient speechClient = SpeechClient.create()) {
+        CredentialsProvider credentialsProvider = FixedCredentialsProvider.create(
+                ServiceAccountCredentials.fromStream(
+                        new FileInputStream("/Users/jaehong/Downloads/drrr-423416-5be1d0478172.json")));
 
+        var settings = SpeechSettings.newBuilder()
+                .setCredentialsProvider(credentialsProvider)
+                .build();
+
+        try (SpeechClient speechClient = SpeechClient.create(settings)) {
             // The path to the audio file to transcribe
 
             // Builds the sync recognize request
             var config = RecognitionConfig.newBuilder()
                     .setEncoding(AudioEncoding.LINEAR16)
                     .setSampleRateHertz(16000)
-                    .setLanguageCode("ko-KR")
+                    .setModel("default")
+                    .setLanguageCode("en-US")
                     .build();
+
             RecognitionAudio audio = RecognitionAudio.newBuilder()
-                    .setContent(ByteString.copyFromUtf8(request.content()))
+                    .setContent(ByteString.readFrom(new FileInputStream("/Users/jaehong/Desktop/무제.m4a")))
                     .build();
 
             // Performs speech recognition on the audio file
             RecognizeResponse response = speechClient.recognize(config, audio);
+            log.info("{}", response);
             List<SpeechRecognitionResult> results = response.getResultsList();
+            log.info("{}", results);
 
             for (SpeechRecognitionResult result : results) {
                 // There can be several alternative transcripts for a given chunk of speech. Just use the
@@ -58,6 +74,7 @@ public class SpeechApi {
                 SpeechRecognitionAlternative alternative = result.getAlternativesList().getFirst();
                 log.info("{}", alternative.getTranscript());
             }
+
             return results.stream()
                     .map(SpeechRecognitionResult::getAlternativesList)
                     .map(List::getFirst)
