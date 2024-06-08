bda5968f7d83d33ef182389c8a072c9b0c8f3577
JaehongDev <println@kakao.com>
2024-06-08 22:26:26 +0900

    feat: question 생성 & 수정 구현

--- backend/src/main/java/in/backend/domain/question/entity/QuestionEntity.java
+++ backend/src/main/java/in/backend/domain/question/entity/QuestionEntity.java
@@ -1,7 +1,9 @@
 package in.backend.domain.question.entity;
 
 
+import in.backend.domain.question.application.QuestionWriter.QuestionInfo;
 import in.backend.global.entity.BaseEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -9,8 +11,10 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Getter
 @Entity
 @Table(name = "questions")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -20,13 +24,18 @@ public class QuestionEntity extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+
+    /**
+     * 질문 내용입니다.
+     */
+    @Column(nullable = false)
     private String content;
 
     /**
-     * 어떤 관리자가 해당 질문을 만들었는지 알고 싶습니다.
+     * 질문 목록에서 순서 정렬 기준이 됨
      */
-    private Long adminId;
-
+    @Column(nullable = false)
+    private Integer sequence;
 
     /**
      * 콤마로 링크를 구분합니다.
@@ -35,11 +44,21 @@ public class QuestionEntity extends BaseEntity {
 
 
     @Builder
-    public QuestionEntity(String content, Long adminId, String referenceLinks) {
+    public QuestionEntity(
+            String content,
+            String referenceLinks,
+            int sequence
+    ) {
         this.content = content;
-        this.adminId = adminId;
         this.referenceLinks = referenceLinks;
+        this.sequence = sequence;
     }
 
+
+    public void update(QuestionInfo questionInfo) {
+        applyIfPresent(questionInfo.content(), value -> this.content = value);
+        applyIfPresent(questionInfo.referenceLinks(), value -> this.referenceLinks = value);
+        applyIfPresent(questionInfo.sequence(), value -> this.sequence = value);
+    }
 
 }
