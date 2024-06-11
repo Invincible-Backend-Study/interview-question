abf603f49811e5f4c4804af01346ff065283b228
JaehongDev <println@kakao.com>
2024-06-11 21:44:34 +0900

    feat: jwtProvider 구현

---
+++ backend/src/main/java/in/backend/global/provider/JwtProvider.java
@@ -0,0 +1,100 @@
+package in.backend.global.provider;
+
+import static in.backend.global.exception.GlobalExceptionCode.EXPIRED_ACCESS_TOKEN;
+import static in.backend.global.exception.GlobalExceptionCode.EXPIRED_REFRESH_TOKEN;
+import static in.backend.global.exception.GlobalExceptionCode.INVALID_ACCESS_TOKEN;
+import static in.backend.global.exception.GlobalExceptionCode.INVALID_REFRESH_TOKEN;
+
+import in.backend.global.exception.InvalidJwtException;
+import in.backend.global.exception.JwtExpiredException;
+import jakarta.servlet.http.HttpServletRequest;
+import java.time.Instant;
+import java.util.Map;
+import java.util.Objects;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.security.oauth2.jwt.JwtValidationException;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class JwtProvider {
+    private static final String issuer = "MUBAEGSEU";
+    private static final Long accessTokenExpiry = (7) * 24 * 60 * 60L; // 1주
+    private static final Long refreshTokenExpiry = (7 * 3) * 24 * 60 * 60L; // 3주
+    private static final String TOKEN_PREFIX = "Bearer ";
+    private static final String HEADER_AUTHORIZATION = "Authorization";
+
+    private final JwtEncoder jwtEncoder;
+    private final JwtDecoder jwtDecoder;
+
+    public String createAccessToken(final Long id, final Instant issuanceTime) {
+        final JwtClaimsSet claims = JwtClaimsSet.builder()
+                .issuer(issuer)
+                .issuedAt(issuanceTime)
+                .expiresAt(issuanceTime.plusSeconds(accessTokenExpiry))
+                .subject(String.valueOf(id))
+                .claim("id", id)
+                .build();
+
+        return jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+    }
+
+    public String createRefreshToken(final Long id, final Instant issuanceTime) {
+        final JwtClaimsSet claims = JwtClaimsSet.builder()
+                .issuer(issuer)
+                .issuedAt(issuanceTime)
+                .expiresAt(issuanceTime.plusSeconds(refreshTokenExpiry))
+                .subject(String.valueOf(id))
+                .claim("id", id)
+                .build();
+        return jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+    }
+
+    private Map<String, Object> decode(final String token) {
+        return jwtDecoder.decode(token).getClaims();
+    }
+
+
+    public Long extractToValueFrom(final String token) {
+        return Long.parseLong(this.decode(token)
+                .get("id")
+                .toString()
+        );
+    }
+
+    public Long decodeAccessToken(final String accessToken) {
+        try {
+            return Long.parseLong(String.valueOf(this.decode(accessToken).get("id")));
+        } catch (JwtValidationException exception) {
+            throw new JwtExpiredException(EXPIRED_ACCESS_TOKEN);
+        } catch (Exception exception) {
+            throw new InvalidJwtException(INVALID_ACCESS_TOKEN);
+        }
+    }
+
+    public Long decodeRefreshToken(final String refreshToken) {
+        try {
+            return Long.parseLong(String.valueOf(this.decode(refreshToken).get("id")));
+        } catch (JwtValidationException exception) {
+            throw new JwtExpiredException(EXPIRED_REFRESH_TOKEN);
+        } catch (Exception exception) {
+            throw new InvalidJwtException(INVALID_REFRESH_TOKEN);
+        }
+    }
+
+    public String extractToken(final HttpServletRequest request) {
+        final String token = request.getHeader(HEADER_AUTHORIZATION);
+
+        if (!Objects.isNull(token) && token.startsWith(TOKEN_PREFIX)) {
+            return token.substring(TOKEN_PREFIX.length());
+        }
+        return null;
+    }
+
+}
