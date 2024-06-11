957e055a06166e3aba757f53645619accf585ffd
JaehongDev <println@kakao.com>
2024-06-11 21:59:27 +0900

    refactor: configurationPropertyScan 설정

--- backend/src/main/java/in/backend/Application.java
+++ backend/src/main/java/in/backend/Application.java
@@ -2,8 +2,10 @@ package in.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan(basePackages = "in.backend")
 public class Application {
 
     public static void main(String... args) {
