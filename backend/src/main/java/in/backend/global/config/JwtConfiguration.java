e7ccc2c3247a5353729636ce9f97ddd9d6b07143
JaehongDev <println@kakao.com>
2024-06-11 21:43:42 +0900

    feat: jwt encoder decoder 정의

---
+++ backend/src/main/java/in/backend/global/config/JwtConfiguration.java
@@ -0,0 +1,38 @@
+package in.backend.global.config;
+
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+import in.backend.global.property.JwtProperty;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+public class JwtConfiguration {
+
+
+    private final JwtProperty jwtProperty;
+
+
+    @Bean
+    JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withPublicKey(jwtProperty.rsaPublicKey()).build();
+    }
+
+    @Bean
+    JwtEncoder jwtEncoder() {
+        final JWK jwk = new RSAKey.Builder(jwtProperty.rsaPublicKey()).privateKey(jwtProperty.rsaPrivateKey()).build();
+        final JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+        return new NimbusJwtEncoder(jwks);
+    }
+}
+
