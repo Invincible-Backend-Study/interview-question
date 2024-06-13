e2ba04ee08b08fefb67b62a16a15a5d2d638479c
JaehongDev <println@kakao.com>
2024-06-13 23:48:13 +0900

    refactor: Report 도메인 제거

--- backend/src/main/java/in/backend/core/report/entity/ReportState.java
+++
@@ -1,7 +0,0 @@
-package in.backend.core.report.entity;
-
-public enum ReportState {
-    PROGRESS, // 생성 즉시 초기화 된 값
-    GIVE_UP, // 포기 또는 오랜 시간 동안 진행하지 않은 경우
-    COMPLETE // 완료
-}
--- backend/src/main/java/in/backend/core/report/repository/ReportItemRepository.java
+++
@@ -1,7 +0,0 @@
-package in.backend.core.report.repository;
-
-import in.backend.core.report.entity.ReportItemEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReportItemRepository extends JpaRepository<ReportItemEntity, Long> {
-}
