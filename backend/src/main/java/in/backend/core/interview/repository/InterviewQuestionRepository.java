d60ba3a26a5e5b000159a7bd90bd8a51f5d9e0ec
JaehongDev <println@kakao.com>
2024-06-14 23:19:31 +0900

    feat: 인터뷰 질문 조회 기능 추가

--- backend/src/main/java/in/backend/core/interview/repository/InterviewQuestionRepository.java
+++ backend/src/main/java/in/backend/core/interview/repository/InterviewQuestionRepository.java
@@ -2,8 +2,12 @@ package in.backend.core.interview.repository;
 
 
 import in.backend.core.interview.entity.InterviewQuestionEntity;
+import java.util.List;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InterviewQuestionRepository extends JpaRepository<InterviewQuestionEntity, Long> {
+
+    List<InterviewQuestionEntity> findByInterviewIdAndMemberId(Long interviewId, Long memberId, Pageable pageable);
 
 }
