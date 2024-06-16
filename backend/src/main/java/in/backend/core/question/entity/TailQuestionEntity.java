e690b2e938dcd1be9571323eaef58e2c6250a5b3
JaehongDev <println@kakao.com>
2024-06-16 19:09:39 +0900

    feat: TailQuestionEntity 저으이

--- backend/src/main/java/in/backend/core/question/entity/TailQuestionEntity.java
+++ backend/src/main/java/in/backend/core/question/entity/TailQuestionEntity.java
@@ -1,14 +1,24 @@
 package in.backend.core.question.entity;
 
 
+import in.backend.core.interview.application.InterviewSubmitCommand.AnswerInfo;
+import in.backend.core.interview.application.InterviewSubmitCommand.FeedbackInfo;
+import in.backend.core.interview.entity.AIFeedback;
+import in.backend.core.interview.entity.Answer;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+
+@Getter
 @Entity
 @Table(name = "tail_questions")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -18,8 +28,51 @@ public class TailQuestionEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private Long reportItemId;
+    @Column(nullable = false)
+    private Long memberId;
 
-    private String content;
+    @Column(nullable = false)
+    private Long interviewId;
 
+    @Column(nullable = false)
+    private Long interviewQuestionId;
+
+    @Column(nullable = false)
+    private String question;
+
+    private AIFeedback aiFeedback;
+
+    @Embedded
+    private Answer answer = Answer.init();
+
+
+    @Builder
+    public TailQuestionEntity(Long interviewId, Long memberId, Long interviewQuestionId, String question) {
+        this.interviewId = interviewId;
+        this.interviewQuestionId = interviewQuestionId;
+        this.memberId = memberId;
+        this.question = question;
+    }
+
+    public void submit(AnswerState answerState, AnswerInfo answerInfo, FeedbackInfo feedbackInfo) {
+        switch (answerState) {
+            case COMPLETE -> answer = Answer.pass();
+            case PASS -> {
+                this.aiFeedback = AIFeedback.from(feedbackInfo);
+                this.answer = Answer.complete(
+                        answerInfo.content(),
+                        answerInfo.timeToAnswer()
+                );
+            }
+            default -> throw new UnsupportedOperationException();
+        }
+    }
+
+    public boolean isComplete() {
+        return answer.isComplete();
+    }
+
+    public String getTailQuestion() {
+        return aiFeedback.getTailQuestion();
+    }
 }
