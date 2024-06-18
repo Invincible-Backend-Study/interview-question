a6019e09e7eb817279aa90a711a02b7396ff96e6
JaehongDev <println@kakao.com>
2024-06-19 01:53:13 +0900

    feat: api endpoint 정의

--- frontend/src/constants/api.ts
+++ frontend/src/constants/api.ts
@@ -1,5 +1,23 @@
 export const NETWORK = {
   RETRY_COUNT: 2,
   TIMEOUT: 10000
-} as const;
+}as const;
 
+
+export const END_POINT = {
+  TAIL_QUESTION_SUBMIT: "/tail-questions/submit",
+
+  INTERVIEWS: "/interviews",
+  INTERVIEWS_SUBMIT: "/interviews/submit",
+  INTERVIEWS_GET_PROBLEM: (interviewId: number) => `/interviews/${interviewId}/current/problem`,
+  INTERVIEWS_ME: "/interviews/me",
+
+  SIGNUP: "/auth/signup",
+  SIGNIN: "/auth/signin",
+  PROFILE: "/auth/profile",
+  LOGOUT: "/auth/logout",
+  TOKEN_REISSUE: "/auth/token/reissue",
+
+  QUESTION_SETS: "/v1/question-set"
+
+} as const;
