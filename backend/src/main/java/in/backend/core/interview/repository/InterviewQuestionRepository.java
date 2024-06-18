89c1293fb1e21d3278e4838c47752310a9a33391
JaehongDev <println@kakao.com>
2024-06-19 01:02:43 +0900

    fix: @Param 애노테이션 추가

--- backend/src/main/java/in/backend/core/interview/repository/InterviewQuestionRepository.java
+++ backend/src/main/java/in/backend/core/interview/repository/InterviewQuestionRepository.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface InterviewQuestionRepository extends JpaRepository<InterviewQuestionEntity, Long> {
 
@@ -19,5 +20,5 @@ public interface InterviewQuestionRepository extends JpaRepository<InterviewQues
                 where I.interviewId in :interviewIds
                 group by I.interviewId
             """)
-    List<InterviewQuestionCount> countByInterviewIds(List<Long> interviewIds);
+    List<InterviewQuestionCount> countByInterviewIds(@Param("interviewIds") List<Long> interviewIds);
 }
