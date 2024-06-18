89ce830e96e6d89c9c9ef0e5d4bca5faeaf9c99e
JaehongDev <println@kakao.com>
2024-06-18 20:57:56 +0900

    feat: api 타입 정의

---
+++ frontend/src/types/questionSet.ts
@@ -0,0 +1,24 @@
+
+
+export interface QuestionSetSearchResponse {
+  /**
+   * question set id
+   */
+  id: number;
+
+  /**
+   * question set title
+   */
+  title: string;
+
+  /**
+   * 설명
+   */
+  description: string;
+
+  /**
+   * problem count
+   */
+  count: number;
+
+}
