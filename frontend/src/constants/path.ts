b59644f1198547b79d0c65ee3b205007dcc76b88
JaehongDev <println@kakao.com>
2024-06-24 12:10:27 +0900

    refactor: profile modal 제거

--- frontend/src/constants/path.ts
+++ frontend/src/constants/path.ts
@@ -4,5 +4,6 @@
 export const PATH = {
   MAIN_PAGE: "/",
   AUTH: "/auth" ,
-  INTERVIEW: (id: number) => `/interviews/${id}`
+  INTERVIEW: (id: number) => `/interviews/${id}`,
+  MY_INTERVIEW: '/history'
 } as const;
--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -2,10 +2,8 @@ import QuestionSetItem from "@/components/QuestionSetItem/QuestionSetItem";
 import {Button, Modal, ScrollShadow, Spacer, useDisclosure,} from "@nextui-org/react";
 import {useQuestionSetQuery} from "@/hooks/api/question/useQuestionSetQuery";
 import {useCallback, useState} from "react";
-import {InterviewCreateFormCommand, InterviewSettings} from "@/types/interview";
+import {InterviewSettings} from "@/types/interview";
 import InterviewCreateForm from "@/components/InterviewCreatorForm/InterviewCreateForm";
-import interviewController from "@/components/InterviewController/InterviewController";
-import interviewCreateForm from "@/components/InterviewCreatorForm/InterviewCreateForm";
 
 export default function(){
   const {questionSetList } = useQuestionSetQuery();
@@ -27,7 +25,6 @@ export default function(){
     <div className="p-3">
       <div>
         <span className="text-3xl">질문 목록</span>
-        <Button onClick={onOpen}>임시</Button>
       </div>
       <Spacer y={10}/>
 
