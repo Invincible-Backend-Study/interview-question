ebc84e11e4cda7be0d8f890955c203c7db63107e
JaehongDev <println@kakao.com>
2024-06-25 23:42:19 +0900

    refactor: 단축키 변경

--- frontend/src/components/InterviewController/InterviewController.tsx
+++ frontend/src/components/InterviewController/InterviewController.tsx
@@ -11,15 +11,15 @@ interface InterviewControllerProps {
 const InterviewController = ({onSubmit, onPass, disabled}: InterviewControllerProps) => {
   return <div className="flex flex-row-reverse gap-3 p-3">
     <Button onClick={onSubmit} isLoading={disabled}>
-      제출 <Kbd keys={["command"]}>S</Kbd>
+      제출 <Kbd keys={["command"]}>s</Kbd>
     </Button>
     <Button onClick={onPass} isLoading={disabled}>
-      패스 <Kbd keys={["option", "command"]}>A</Kbd>
+      패스 <Kbd keys={["command"]}>p</Kbd>
     </Button>
     <Button isDisabled>일시정지</Button>
     <Button>
       그만두기
-      <Kbd keys={["option", "command"]}>P</Kbd>
+      <Kbd keys={["command"]}>q</Kbd>
     </Button>
   </div>
 }
