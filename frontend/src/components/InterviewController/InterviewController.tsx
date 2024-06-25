8f2f0afd6644bb6ceecb132b6c3ed3f1af3aab0f
JaehongDev <println@kakao.com>
2024-06-25 20:09:02 +0900

    refactor: 버튼 로딩 구현

--- frontend/src/components/InterviewController/InterviewController.tsx
+++ frontend/src/components/InterviewController/InterviewController.tsx
@@ -4,15 +4,16 @@ import {Button,  Kbd} from "@nextui-org/react";
 interface InterviewControllerProps {
   onSubmit: () => void
   onPass: () => void
+  disabled: boolean
 }
 
 
-const InterviewController = ({onSubmit, onPass}: InterviewControllerProps) => {
+const InterviewController = ({onSubmit, onPass, disabled}: InterviewControllerProps) => {
   return <div className="flex flex-row-reverse gap-3 p-3">
-    <Button onClick={onSubmit}>
-      제출 <Kbd keys={["option", "command"]}>S</Kbd>
+    <Button onClick={onSubmit} isLoading={disabled}>
+      제출 <Kbd keys={["command"]}>S</Kbd>
     </Button>
-    <Button onClick={onPass}>
+    <Button onClick={onPass} isLoading={disabled}>
       패스 <Kbd keys={["option", "command"]}>A</Kbd>
     </Button>
     <Button isDisabled>일시정지</Button>
