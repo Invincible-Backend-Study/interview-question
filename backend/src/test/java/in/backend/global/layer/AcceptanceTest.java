56690516416b329e7b69811a0c7c4edf94ed4bc6
JaehongDev <println@kakao.com>
2024-06-27 18:41:06 +0900

    test: 인수 테스트에서 로그인 요청 테스트 시작 전 호출하도록 변경

--- backend/src/test/java/in/backend/global/layer/AcceptanceTest.java
+++ backend/src/test/java/in/backend/global/layer/AcceptanceTest.java
@@ -1,19 +1,29 @@
 package in.backend.global.layer;
 
 
-import static io.grpc.netty.shaded.io.netty.handler.codec.http.HttpHeaders.Values.APPLICATION_JSON;
+import static io.netty.handler.codec.http.HttpHeaders.Values.APPLICATION_JSON;
 
 import in.backend.core.auth.presentation.payload.response.AccessTokenResponse;
 import in.backend.global.fixture.AuthApiFixture;
+import in.backend.global.layer.AcceptanceTest.ConditionalLoginRunner;
 import in.backend.global.utils.DatabaseCleaner;
-import io.grpc.netty.shaded.io.netty.handler.codec.http.HttpHeaderNames;
+import io.netty.handler.codec.http.HttpHeaderNames;
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.internal.RequestSpecificationImpl;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.util.Arrays;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.extension.BeforeTestExecutionCallback;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.ExtensionContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
@@ -21,9 +31,10 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
 
 
+@ActiveProfiles("test")
+@ExtendWith(ConditionalLoginRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
-@ActiveProfiles("test")
 public class AcceptanceTest {
 
     @LocalServerPort
@@ -36,22 +47,10 @@ public class AcceptanceTest {
     void setup() {
         databaseCleaner.execute();
         RestAssured.port = port;
-        var response = RestAssured.given().log().all()
-                .body(AuthApiFixture.signupRequest())
-                .contentType(APPLICATION_JSON)
-                .when()
-                .post("/api/auth/signup")
-                .then()
-                .extract();
-
-        var accessToken = response.jsonPath()
-                .getObject(".", AccessTokenResponse.class)
-                .accessToken();
+        System.out.println("check");
 
         RestAssured.requestSpecification = new RequestSpecBuilder()
                 .addHeader(HttpHeaderNames.CONTENT_TYPE.toString(), APPLICATION_JSON)
-                .addHeader(HttpHeaderNames.AUTHORIZATION.toString(), STR."Bearer \{accessToken}")
-                .addCookies(response.cookies())
                 .addHeader(HttpHeaderNames.ACCEPT.toString(), APPLICATION_JSON)
                 .build();
     }
@@ -62,4 +61,50 @@ public class AcceptanceTest {
     }
 
 
+    @Target({ElementType.METHOD, ElementType.TYPE})
+    @Retention(RetentionPolicy.RUNTIME)
+    public @interface LogIn {
+
+    }
+
+    public static class ConditionalLoginRunner implements BeforeTestExecutionCallback {
+        @Override
+        public void beforeTestExecution(ExtensionContext extensionContext) throws Exception {
+
+            Stream.concat(
+                            Arrays.stream(extensionContext.getTestClass()
+                                    .orElseThrow()
+                                    .getAnnotations()),
+                            Arrays.stream(extensionContext.getTestMethod()
+                                    .orElseThrow()
+                                    .getAnnotations())
+                    )
+                    .filter(annotation -> annotation.annotationType().equals(LogIn.class))
+                    .findFirst()
+                    .ifPresent((_) -> signupAndLogin());
+
+        }
+
+        private void signupAndLogin() {
+            var response = RestAssured.given().log().all()
+                    .body(AuthApiFixture.signupRequest())
+                    .contentType(APPLICATION_JSON)
+                    .when()
+                    .post("/api/auth/signup")
+                    .then()
+                    .extract();
+
+            var accessToken = response.jsonPath()
+                    .getObject(".", AccessTokenResponse.class)
+                    .accessToken();
+
+            RestAssured.requestSpecification = new RequestSpecBuilder()
+                    .addHeader(HttpHeaderNames.CONTENT_TYPE.toString(), APPLICATION_JSON)
+                    .addHeader(HttpHeaderNames.AUTHORIZATION.toString(), STR."Bearer \{accessToken}")
+                    .addCookies(response.cookies())
+                    .addHeader(HttpHeaderNames.ACCEPT.toString(), APPLICATION_JSON)
+                    .build();
+        }
+
+    }
 }
