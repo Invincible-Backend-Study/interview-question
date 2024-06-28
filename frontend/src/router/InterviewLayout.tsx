2f31b2e86c9ea8d88daa7fa305c418581894c4d0
JaehongDev <println@kakao.com>
2024-06-28 22:46:26 +0900

    refactor: InterviewLayout Error boundary 추가

--- frontend/src/router/InterviewLayout.tsx
+++ frontend/src/router/InterviewLayout.tsx
@@ -3,6 +3,27 @@ import {useRecoilState} from "recoil";
 import {isLoggedInState} from "@/store/auth";
 import {useEffect} from "react";
 import {PATH} from "@/constants/path";
+import {ErrorBoundary, FallbackProps} from "react-error-boundary";
+import {HTTPError} from "@/api/Interceptors";
+
+const FallbackComponent = ({ error, resetErrorBoundary }: FallbackProps) => {
+
+  console.log(">>>", error);
+  if(error instanceof  HTTPError) {
+    if(error.code === 403) {
+      location.assign(PATH.MAIN_PAGE);
+      return;
+    }
+  }
+
+  return (
+    <div>
+      <h2>잘못된 접근 방식</h2>
+      <p>{error.message}</p>
+      <button onClick={resetErrorBoundary}>다시 시도</button>
+    </div>
+  );
+};
 
 
 const InterviewLayout = () => {
@@ -10,16 +31,18 @@ const InterviewLayout = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
+    console.log(">>>>", isLoggedIn);
     if(!isLoggedIn) {
       navigate(PATH.MAIN_PAGE)
     }
   }, [isLoggedIn])
 
   return (
-    <>
+    <ErrorBoundary fallbackRender={FallbackComponent}>
       <Outlet/>
-    </>
+    </ErrorBoundary>
   )
 }
+
 
 export default InterviewLayout;
