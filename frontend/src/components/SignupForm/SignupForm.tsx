357e302cd87b383fbb0605640e532f374c8cbb05
JaehongDev <println@kakao.com>
2024-07-02 19:08:11 +0900

    refactor: 인증 폼 불필요한 ui 제거

--- frontend/src/components/SignupForm/SignupForm.tsx
+++ frontend/src/components/SignupForm/SignupForm.tsx
@@ -1,5 +1,5 @@
-import {Button, Card, CardBody, CardFooter, CardHeader, Chip, Input, Spacer} from "@nextui-org/react";
-import {useCallback, useEffect} from "react";
+import {Button, Card, CardBody, CardFooter, CardHeader, Input, Spacer} from "@nextui-org/react";
+import {useEffect} from "react";
 import {useOAuthProfileQuery} from "@/hooks/api/auth/useOAuthProfileQuery";
 import {useSignupForm} from "@/components/SignupForm/useSignupForm";
 import {useSignInMutation} from "@/hooks/api/auth/useSignInMutation";
@@ -14,10 +14,6 @@ const SignupForm = ({providerId}: SignupFormProps) => {
   const {memberInfo, updateInputValue, handleSignup} = useSignupForm(profile);
   const signInMutation = useSignInMutation();
 
-  const handleRedirectGithubRepository = useCallback(() => {
-    location.assign("https://github.com/invincible-Backend-Study");
-  },[])
-
   useEffect(() => {
     if(profile.isRegistered) {
       signInMutation.mutate({
@@ -28,15 +24,13 @@ const SignupForm = ({providerId}: SignupFormProps) => {
 
 
   return (
-    signInMutation.isPaused && (
+    !profile.isRegistered && (
       <Card className="p-4 bg-default-100 w-[40vw]" >
         <CardHeader className="flex justify-between ">
           <div className="flex">
-            <span className="text-2xl">회원가입</span>
+            <span className="text-2xl text-success">회원가입</span>
             <Spacer x={2} />
-            <Chip>MEMBER</Chip>
           </div>
-          <Button color="primary" onClick={handleRedirectGithubRepository}>무적 백엔드 스터디 리포지터리 보러가기</Button>
         </CardHeader>
         <CardBody>
           <p>
@@ -44,11 +38,14 @@ const SignupForm = ({providerId}: SignupFormProps) => {
             편하게 이용 가능하지만, 비용 문제로 사라질 수 있습니다~
           </p>
         </CardBody>
-        <CardFooter>
-          <Input color="primary" placeholder="닉네임" value={memberInfo.nickname} onChange={(e) => updateInputValue('nickname', e.target.value)}/>
-          <div>
-            <Button onClick={handleSignup}>확인</Button>
-          </div>
+        <CardFooter className="gap-3">
+          <form className="flex flex-row gap-3 w-full" onSubmit={(event) => {
+            event.preventDefault();
+            handleSignup();
+          }}>
+          <Input color="primary" placeholder="닉네임" value={memberInfo.nickname} radius="none" onChange={(e) => updateInputValue('nickname', e.target.value)}/>
+          <Button type={"submit"}   radius="none">확인</Button>
+          </form>
         </CardFooter>
       </Card>
     )
