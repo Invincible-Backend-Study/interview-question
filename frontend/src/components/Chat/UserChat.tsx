2cf71e2c7556f57efae624ea6be4d160a84eab97
JaehongDev <println@kakao.com>
2024-07-12 01:05:45 +0900

    feat: 모바일 chat 구현

--- frontend/src/components/Chat/UserChat.tsx
+++ frontend/src/components/Chat/UserChat.tsx
@@ -2,11 +2,10 @@ import {Answer} from "@/types/question";
 
 const UserChat = ({content, isError}: Answer) => {
   return (
-    <div className={`flex flex-row-reverse gap-2.5 ${isError ? "text-warning" : ""}`}>
-      {/*<img className='w-8 h-8 rounded-full' src='/docs/images/people/profile-picture-3.jpg' alt='Jese image'/>*/}
+    <div className={`flex flex-row-reverse gap-2.5 ${isError ? "text-warning" : ""} `}>
       <div
-        className='flex flex-col w-full max-w-[50vw] leading-1.5 p-4 border-gray-200 bg-gray-100 rounded-e-xl rounded-es-xl dark:bg-gray-700'>
-        <p className={`text-sm font-normal py-2.5 text-gray-900 ${isError ? "text-warning" : "text-white"}`}>
+        className='flex flex-col w-full max-w-[50vw] leading-1.5 p-4 rounded-l-xl rounded-b-xl rounded-es-xl dark:bg-gray-800'>
+        <p className={`text-sm font-normal py-2.5 ${isError ? "text-warning" : "text-white"}`}>
           {content}
         </p>
       </div>
